c5c82369705d5b21b5883b187faf66cd1421df3f
mazzampr <moh.azam121@gmail.com>
2023-09-25 00:32:56 +0700

    change profile ui

--- app/src/main/java/com/upnvjt/trashcare/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/upnvjt/trashcare/ui/main/profile/ProfileFragment.kt
@@ -42,7 +42,7 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.linearLogOut.setOnClickListener {
+        binding.btnLogout.setOnClickListener {
             lifecycleScope.launch {
                 googleAuthUiClient.signOut()
             }
